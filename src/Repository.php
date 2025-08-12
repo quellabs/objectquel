573af4d68e43083959fac03b8349d5d2108db442
noescom <E934BFdkKtWndW7>
2025-08-12 07:49:39 +0200

    Removed some doc comments

--- src/Repository.php
+++ src/Repository.php
@@ -7,12 +7,6 @@
 	 * featuring a unique query language, a streamlined architecture, and powerful
 	 * entity relationship management. It implements the Data Mapper pattern for
 	 * clear separation between domain models and underlying database structures.
-	 *
-	 * @author      Floris van den Berg
-	 * @copyright   Copyright (c) 2025 ObjectQuel
-	 * @license     MIT
-	 * @version     1.0.0
-	 * @package     Quellabs\ObjectQuel
 	 */
 
 	namespace Quellabs\ObjectQuel;
