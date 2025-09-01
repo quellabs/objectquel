b6cfef3b0b3a99b3f5a1692c55212d962ab5cfe4
noescom <E934BFdkKtWndW7>
2025-09-01 23:23:26 +0200

    Added parentContext to ReferenceAggregate and ReferenceAggregateWhere.

--- src/Execution/RangeReferences/ReferenceAggregate.php
+++ src/Execution/RangeReferences/ReferenceAggregate.php
@@ -5,4 +5,15 @@
 	use Quellabs\ObjectQuel\ObjectQuel\Ast\AstIdentifier;
 	
 	class ReferenceAggregate extends Reference{
+		
+		private string $parentContext;
+		
+		public function __construct(AstIdentifier $identifier, string $parentContext) {
+			parent::__construct($identifier);
+			$this->parentContext = $parentContext;
+		}
+		
+		public function getParentContext(): string {
+			return $this->parentContext;
+		}
 	}
