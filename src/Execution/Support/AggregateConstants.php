f3c47fc25b51fc95c082c8271dc7c469e3666240
noescom <E934BFdkKtWndW7>
2025-09-04 16:22:19 +0200

    Optimizations

--- src/Execution/Support/AggregateConstants.php
+++ src/Execution/Support/AggregateConstants.php
@@ -26,6 +26,15 @@
 			AstMax::class,
 		];
 		
+		/** @var array<class-string<AstInterface>> NOT-DISTINCT-capable aggregate classes. */
+		public const array NOT_DISTINCT_AGGREGATE_TYPES = [
+			AstSum::class,
+			AstCount::class,
+			AstAvg::class,
+			AstMin::class,
+			AstMax::class
+		];
+
 		/** @var array<class-string<AstInterface>> DISTINCT-capable aggregate classes. */
 		public const array DISTINCT_AGGREGATE_TYPES = [
 			AstSumU::class,
