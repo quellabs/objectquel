e519d15f02d4e5c4100d501a5931fef5a056b5af
noescom <E934BFdkKtWndW7>
2025-11-27 10:19:47 +0100

    Improved comments

--- src/ObjectQuel/Visitors/NodeCollector.php
+++ src/ObjectQuel/Visitors/NodeCollector.php
@@ -5,37 +5,30 @@
 	use Quellabs\ObjectQuel\ObjectQuel\AstInterface;
 	use Quellabs\ObjectQuel\ObjectQuel\AstVisitorInterface;
 	
+	/**
+	 * Collects AST nodes matching specified types during tree traversal
+	 */
 	class NodeCollector implements AstVisitorInterface {
 		
-		/** @var array The node types to search for */
-		private array $types;
-		
-		/** @var array All nodes */
-		private array $collectedNodes;
+		private array $targetTypes;
+		private array $nodes = [];
 		
 		/**
-		 * CollectNodes constructor
-		 * @param array|string $types
+		 * NodeCollector constructor
+		 * @param string|array $types Node class name(s) to collect
 		 */
-		public function __construct(array|string $types) {
-			if (is_string($types)) {
-				$this->types = [$types];
-			} else {
-				$this->types = $types;
-			}
-
-			$this->collectedNodes = [];
+		public function __construct(string|array $types) {
+			$this->targetTypes = is_array($types) ? $types : [$types];
 		}
 		
 		/**
-		 * Visits a node in the AST
-		 * @param AstInterface $node The current node being visited
+		 * Collects node if it matches any target type
+		 * @param AstInterface $node The node being visited
 		 */
 		public function visitNode(AstInterface $node): void {
-			// Skip if not an AstIdentifier node
-			foreach($this->types as $type) {
+			foreach ($this->targetTypes as $type) {
 				if (is_a($node, $type)) {
-					$this->collectedNodes[] = $node;
+					$this->nodes[] = $node;
 					return;
 				}
 			}
@@ -43,9 +36,9 @@
 		
 		/**
 		 * Returns all collected nodes
-		 * @return array
+		 * @return AstInterface[]
 		 */
-		public function getCollectedNodes(): array {
-			return $this->collectedNodes;
+		public function getNodes(): array {
+			return $this->nodes;
 		}
 	}
