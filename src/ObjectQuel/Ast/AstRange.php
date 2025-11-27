6fe2b3e91a2cd7a22596763ff15f3adf33e3e453
noescom <E934BFdkKtWndW7>
2025-11-27 10:07:30 +0100

    Renamed FindIdentifier.php to IdentifierLocator.php

--- src/ObjectQuel/Ast/AstRange.php
+++ src/ObjectQuel/Ast/AstRange.php
@@ -4,7 +4,7 @@
 	
 	use Quellabs\ObjectQuel\ObjectQuel\AstInterface;
 	use Quellabs\ObjectQuel\ObjectQuel\AstVisitorInterface;
-	use Quellabs\ObjectQuel\ObjectQuel\Visitors\FindIdentifier;
+	use Quellabs\ObjectQuel\ObjectQuel\Visitors\IdentifierLocator;
 	
 	/**
 	 * AstRange class is responsible for defining a range in the AST (Abstract Syntax Tree).
--- src/ObjectQuel/Visitors/IdentifierLocator.php
+++ src/ObjectQuel/Visitors/IdentifierLocator.php
@@ -14,7 +14,7 @@
 	 * within an Abstract Syntax Tree (AST). It traverses the AST looking for an AstIdentifier
 	 * node that matches both the provided entity name and property.
 	 */
-	class FindIdentifier implements AstVisitorInterface {
+	class IdentifierLocator implements AstVisitorInterface {
 		
 		/** @var string The entity name to search for */
 		private string $entityName;
