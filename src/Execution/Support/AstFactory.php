6fd65356babb5559890856afef2b4f85c69c03f8
noescom <E934BFdkKtWndW7>
2025-12-02 10:48:15 +0100

    Fixes

--- src/Execution/Support/AstFactory.php
+++ src/Execution/Support/AstFactory.php
@@ -15,9 +15,9 @@
 		 * Creates a binary OR operator AST node.
 		 * @param AstInterface $left The left operand of the OR operation
 		 * @param AstInterface $right The right operand of the OR operation
-		 * @return AstInterface An AST node representing the OR operation
+		 * @return AstBinaryOperator An AST node representing the OR operation
 		 */
-		public static function createBinaryOrOperator(AstInterface $left, AstInterface $right): AstInterface {
+		public static function createBinaryOrOperator(AstInterface $left, AstInterface $right): AstBinaryOperator {
 			return new AstBinaryOperator($left, $right, 'OR');
 		}
 		
@@ -25,18 +25,18 @@
 		 * Creates a binary AND operator AST node.
 		 * @param AstInterface $left The left operand of the AND operation
 		 * @param AstInterface $right The right operand of the AND operation
-		 * @return AstInterface An AST node representing the AND operation
+		 * @return AstBinaryOperator An AST node representing the AND operation
 		 */
-		public static function createBinaryAndOperator(AstInterface $left, AstInterface $right): AstInterface {
+		public static function createBinaryAndOperator(AstInterface $left, AstInterface $right): AstBinaryOperator {
 			return new AstBinaryOperator($left, $right, 'AND');
 		}
 		
 		/**
 		 * Creates a number literal AST node.
 		 * @param int $value The integer value to be represented in the AST
-		 * @return AstInterface An AST node representing the number literal
+		 * @return AstNumber An AST node representing the number literal
 		 */
-		public static function createNumber(int $value): AstInterface {
+		public static function createNumber(int $value): AstNumber {
 			return new AstNumber((string)$value);
 		}
 	}
