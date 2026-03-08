1e7bfd32fbc9843af22f8b98d069ffaf8967b4f3
noescom <E934BFdkKtWndW7>
2026-03-08 08:58:33 +0100

    Added comments to ProxyInterface

--- src/ProxyGenerator/ProxyInterface.php
+++ src/ProxyGenerator/ProxyInterface.php
@@ -4,10 +4,28 @@
 	
 	use Quellabs\ObjectQuel\EntityManager;
 	
+	/**
+	 * Interface for lazy-loading proxy objects that wrap ORM entities.
+	 */
 	interface ProxyInterface {
-	
+		
+		/**
+		 * Constructor
+		 * @param EntityManager $entityManager Used to load the entity's data on first access.
+		 */
 		public function __construct(EntityManager $entityManager);
+		
+		/**
+		 * Returns whether the proxy has been hydrated with entity data.
+		 * @return bool True if the underlying entity has been loaded from the database.
+		 */
 		public function isInitialized(): bool;
+		
+		/**
+		 * Marks the proxy as initialized, preventing redundant database queries
+		 * on subsequent property accesses.
+		 * @return void
+		 */
 		public function setInitialized(): void;
-
+		
 	}
